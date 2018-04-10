1e50dc97bc0852ef42d874d1f2fb792e30303cc6
Arytur <artur.kozera@gmail.com>
2018-04-10 19:35:45 +0200

    test for selection sort

--- select_sort.py
+++ select_sort.py
@@ -1,4 +1,8 @@
+import unittest
+import random
+
 # Python program for sorting an array by selection
+
 
 # Find the smallest element in the given array
 def find_smallest(array):
@@ -16,6 +20,7 @@ def find_smallest(array):
     # Return index of the smallest number to the 'selection_sort' function
     return smallest_index
 
+
 # Create a new array for the sorted numbers
 def selection_sort(array):
     new_array = []
@@ -31,5 +36,25 @@ def selection_sort(array):
 
 print('\n[5, 3, 6, 2, 10]')
 print(selection_sort([5, 3, 6, 2, 10]))
-print('\n[51, 301, 16, 222, 1010, 44, 119, 1005, 2013, 1, 0, 13, 169]')
-print(selection_sort([51, 301, 16, 222, 1010, 44, 119, 1005, 2013, 1, 0, 13, 169]))
+random_array = [random.randint(0, 2550) for x in range(30)]
+print('\n', random_array)
+print(selection_sort(random_array))
+
+
+class TestSelectionSort(unittest.TestCase):
+
+    test_array1 = [5, 12, 2, 88, 10, 103]
+    test_array2 = [51, 301, 16, 222, 1010, 44, 119, 1005, 2013, 1, 0, 13, 169]
+
+    def test_find_smallest(self):
+        self.assertEqual(find_smallest(self.test_array1), 2)
+        self.assertEqual(find_smallest(self.test_array1[:1]), 0)
+
+        self.assertEqual(find_smallest(self.test_array2), 10)
+        self.assertEqual(find_smallest(self.test_array2[:5]), 2)
+
+    def test_selection_sort(self):
+        self.assertEqual(selection_sort(self.test_array1), [2, 5, 10, 12, 88, 103])
+        self.assertEqual(selection_sort(self.test_array2),
+                         [0, 1, 13, 16, 44, 51, 119, 169, 222, 301, 1005, 1010, 2013])
+
